f77f019f59f637c78a1e73515f7840721ee0034d
snorlax <snorlax@pokemonism.dev>
2025-08-27 12:13:40 +0900

    Define latios subscription.

---
+++ pokemonism/pokemon/latios/subscription.hh
@@ -0,0 +1,30 @@
+/**
+ * @file        pokemon/latios/subscription.hh
+ * @brief
+ * @details
+ *
+ * @author      snorlax <snorlax@pokemonism.dev>
+ * @since       Aug 27, 2025
+ */
+
+#ifndef   __POKEMONISM_POKEMON_LATIOS_SUBSCRIPTION__HH__
+#define   __POKEMONISM_POKEMON_LATIOS_SUBSCRIPTION__HH__
+
+#include <pokemon/latios.hh>
+
+#include <pokemon/mareep/subscription.hh>
+
+namespace pokemon { namespace latios {
+
+    class subscription : public mareep::subscription {
+    protected:  subscription(void) {};
+    protected:  ~subscription(void) override {};
+    public:     subscription(const subscription & o) = delete;
+    public:     subscription(subscription && o) noexcept = delete;
+    public:     subscription & operator=(const subscription & o) = delete;
+    public:     subscription & operator=(subscription && o) noexcept = delete;
+    };
+
+} }
+
+#endif // __POKEMONISM_POKEMON_LATIOS_SUBSCRIPTION__HH__
