3de2e481ba79a80f244e455b1e925306b57056f8
snorlax <snorlax@pokemonism.dev>
2025-08-27 12:09:36 +0900

    Define external latios event class.

--- pokemonism/pokemon/latios/event.hh
+++ pokemonism/pokemon/latios/event.hh
@@ -10,44 +10,16 @@
 #ifndef   __POKEMONISM_POKEMON_LATIOS_EVENT__HH__
 #define   __POKEMONISM_POKEMON_LATIOS_EVENT__HH__
 
-#include <pokemonism.hh>
-#include <pokemon/latios.hh>
-#include <pokemon/latios/queue.hh>
-#include <pokemon/latios/linked/list.hh>
-
 namespace pokemon { namespace latios {
 
     class event {
-    public:     inline static event * cancel(event * e);
-    protected:  queue * container;
-    protected:  event * prev;
-    protected:  event * next;
-    public:     virtual event * on(void) = 0;
-    public:     virtual event * cancel(void) = 0;
-    public:     inline event(void);
-    public:     inline virtual ~event(void);
+    public:     inline event(void) {}
+    public:     inline virtual ~event(void) {}
     public:     event(const event & o) = delete;
     public:     event(event && o) noexcept = delete;
     public:     event & operator=(const event & o) = delete;
     public:     event & operator=(event && o) noexcept = delete;
-    public:     friend class linked::list;
     };
-
-    /** STATIC METHOD */
-    event * event::cancel(event * e) {
-        if (e != nullptr) return e->cancel();
-        return e;
-    }
-
-    /** DEFAULT EVENT METHOD */
-
-    event::event(void) : container(nullptr), prev(nullptr), next(nullptr) {
-
-    }
-
-    event::~event(void) {
-        allocator::del(linked::list::del(this->container, this));
-    }
 
 } }
 
