7e3b4b2fdeec0cafd84d91af0a58d90d26d60123
snorlax <snorlax@pokemonism.dev>
2025-08-27 12:15:41 +0900

    Define latios internal

---
+++ pokemonism/pokemon/latios/internal.cc
@@ -0,0 +1,10 @@
+/**
+ * @file        pokemon/latios/internal.cc
+ * @brief
+ * @details
+ *
+ * @author      snorlax <snorlax@pokemonism.dev>
+ * @since       Aug 27, 2025
+ */
+
+#include "internal.hh"
---
+++ pokemonism/pokemon/latios/internal.hh
@@ -0,0 +1,23 @@
+/**
+ * @file        pokemon/latios/internal.hh
+ * @brief
+ * @details
+ *
+ * @author      snorlax <snorlax@pokemonism.dev>
+ * @since       Aug 27, 2025
+ */
+
+#ifndef   __POKEMONISM_POKEMON_LATIOS_INTERNAL__HH__
+#define   __POKEMONISM_POKEMON_LATIOS_INTERNAL__HH__
+
+namespace pokemon { namespace latios { namespace internal {
+
+    class event;
+    class object;
+    class engine;
+    class generator;
+    class subscription;
+
+} } }
+
+#endif // __POKEMONISM_POKEMON_LATIOS_INTERNAL__HH__
