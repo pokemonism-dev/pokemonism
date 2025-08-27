bae3ed2ff47e2f2f42e00f0a2cb07eb5669ae686
snorlax <snorlax@pokemonism.dev>
2025-08-27 12:18:26 +0900

    latios command.

---
+++ pokemonism/pokemon/latios/command.cc
@@ -0,0 +1,10 @@
+/**
+ * @file        pokemon/latios/command.cc
+ * @brief
+ * @details
+ *
+ * @author      snorlax <snorlax@pokemonism.dev>
+ * @since       Aug 27, 2025
+ */
+
+#include "command.hh"
---
+++ pokemonism/pokemon/latios/command.hh
@@ -0,0 +1,21 @@
+/**
+ * @file        pokemon/latios/command.hh
+ * @brief
+ * @details
+ *
+ * @author      snorlax <snorlax@pokemonism.dev>
+ * @since       Aug 27, 2025
+ */
+
+#ifndef   __POKEMONISM_POKEMON_LATIOS__COMMAND__HH__
+#define   __POKEMONISM_POKEMON_LATIOS__COMMAND__HH__
+
+#include <pokemon/latios.hh>
+
+#include <pokemon/latios/object.hh>
+
+namespace pokemon { namespace latios { namespace command {
+
+} } }
+
+#endif // __POKEMONISM_POKEMON_LATIOS__COMMAND__HH__
