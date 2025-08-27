83f1c2316f50a68e84d8f5f3a843aa9a17189410
snorlax <snorlax@pokemonism.dev>
2025-08-27 12:13:20 +0900

    Define latios object.

---
+++ pokemonism/pokemon/latios/object.hh
@@ -0,0 +1,29 @@
+/**
+ * @file        pokemon/latios/object.hh
+ * @brief
+ * @details
+ *
+ * @author      snorlax <snorlax@pokemonism.dev>
+ * @since       Aug 27, 2025
+ */
+
+#ifndef   __POKEMONISM_POKEMON_LATIOS_OBJECT__HH__
+#define   __POKEMONISM_POKEMON_LATIOS_OBJECT__HH__
+
+#include <pokemon/latios.hh>
+
+namespace pokemon { namespace latios {
+
+    class object : public virtual pokemon::object {
+    protected:  subscription * subscription;
+    public:     object(void) ;
+    public:     ~object(void) override;
+    public:     inline object(const object & o) = delete;
+    public:     inline object(object && o) noexcept = delete;
+    public:     inline object & operator=(const object & o) = delete;
+    public:     inline object & operator=(object && o) noexcept = delete;
+    };
+
+} }
+
+#endif // __POKEMONISM_POKEMON_LATIOS_OBJECT__HH__
