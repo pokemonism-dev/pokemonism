4d58b6ca3bc0b2ac71bcee3610c839845cdbe239
snorlax <snorlax@pokemonism.dev>
2025-08-31 02:51:24 +0900

    Update

---
+++ pokemonism/latios.hh
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           8월 31, 2025
+ */
+
+#ifndef   __POKEMONISM_LATIOS_HH__
+#define   __POKEMONISM_LATIOS_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism {
+}
+
+#endif // __POKEMONISM_LATIOS_HH__
