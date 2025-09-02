76dae55fadc41e07889c10b59fd4d27d06e9f9b4
snorlax <snorlax@pokemonism.dev>
2025-09-02 18:28:18 +0900

    Update Engine.

--- pokemonism/ralts/kirlia/gardevoir/event.cc
+++ pokemonism/ralts/kirlia/gardevoir/event.cc
@@ -10,5 +10,45 @@
 
 #include "event.hh"
 
+#include "node.hh"
+#include "queue.hh"
+
 namespace pokemonism {
+    namespace gardevoir {
+
+        gardevoir::event * event::rel(gardevoir::event * event) {
+            pokemon_develop_check(event == nullptr, return nullptr);
+
+            if (event->container != nullptr) {
+                event->container->del(event);
+
+                if (event->on() > declaration::success) {
+                    if (event->node != nullptr) {
+                        event->node->raise(new pokemon::exception());
+                        event->node->event = nullptr;
+                        event->node = allocator::del(event->node);
+                        return event;
+                    }
+                }
+            }
+
+            if (event->node != nullptr) {
+                event->node->event = nullptr;
+                event->node = allocator::del(event->node);
+            }
+
+            return event;
+        }
+
+        gardevoir::event * event::dispatch(gardevoir::event * event) {
+            pokemon_develop_check(event == nullptr || event->container != nullptr || event->node == nullptr, return nullptr);
+
+            if (event->on() > declaration::success) return event;
+
+            event->node->event = nullptr;
+            event->node = allocator::del(event->node);
+
+            return allocator::del(event);
+        }
+    }
 }
--- pokemonism/ralts/kirlia/gardevoir/event.hh
+++ pokemonism/ralts/kirlia/gardevoir/event.hh
@@ -10,18 +10,48 @@
 #ifndef   __POKEMONISM_GARDEVOIR_EVENT_HH__
 #define   __POKEMONISM_GARDEVOIR_EVENT_HH__
 
+#include <pokemonism/psyduck/linked/list.hh>
+
 #include <pokemonism/ralts/kirlia/event.hh>
+
+#include <pokemonism/ralts/kirlia/gardevoir/node.hh>
 
 namespace pokemonism {
     namespace gardevoir {
 
+        class queue;
+
         class event : public virtual kirlia::event {
-        public:     event(void) {}
-        public:     ~event(void) override {}
+        public:     typedef psyduck::linked::list<gardevoir::queue, gardevoir::event> collection;
+        public:     static gardevoir::event * rel(gardevoir::event * event);
+        public:     static gardevoir::event * dispatch(gardevoir::event * event);
+        protected:  gardevoir::queue *  container;
+        protected:  gardevoir::event *  prev;
+        protected:  gardevoir::event *  next;
+        protected:  gardevoir::node *   node;
+        protected:  uint32              id;
+        public:     inline uint32 eventGet(void) const override { return id; }
+        public:     inline void raise(pokemon::exception * e) const {
+                        pokemon_develop_check(node == nullptr, do {
+                            e = allocator::del(e);
+                            return;
+                        } while (0));
+
+                        node->raise(e);
+                    }
+        public:     inline int on(void) const {
+                        pokemon_develop_check(node == nullptr, return fail);
+
+                        return node->on();
+                    }
+        public:     explicit event(uint32 id, gardevoir::node * node) : container(nullptr), prev(nullptr), next(nullptr), node(node), id(id) { pokemon_develop_quick_check(node == nullptr); }
+        protected:  event(void) : container(nullptr), prev(nullptr), next(nullptr), node(nullptr), id(declaration::invalid) {}
+        public:     ~event(void) override { gardevoir::event::rel(this); }
         public:     event(const gardevoir::event & o) = delete;
         public:     event(gardevoir::event && o) noexcept = delete;
         public:     gardevoir::event & operator=(const gardevoir::event & o) = delete;
         public:     gardevoir::event & operator=(gardevoir::event && o) noexcept = delete;
+        public:     friend collection;
         };
 
     }
