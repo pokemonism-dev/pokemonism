30181249d93f2ae858e1b718b55b572c0292b2e6
snorlax <snorlax@pokemonism.dev>
2025-09-02 10:17:19 +0900

    Update Engine.

---
+++ pokemonism/pokemon/log.hh
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 02, 2025
+ */
+
+#ifndef   __POKEMONISM_POKEMON_LOG_HH__
+#define   __POKEMONISM_POKEMON_LOG_HH__
+
+#include <cstdio>
+
+namespace pokemonism {
+    namespace pokemon {
+
+    }
+}
+
+#define pokemonLog              printf
+
+#endif // __POKEMONISM_POKEMON_LOG_HH__
