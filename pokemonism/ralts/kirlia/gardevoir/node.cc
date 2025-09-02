251b59ea95d319159824c306d579fff4506f0ab6
snorlax <snorlax@pokemonism.dev>
2025-09-02 18:41:58 +0900

    Update Engine.

---
+++ pokemonism/ralts/kirlia/gardevoir/node.cc
@@ -0,0 +1,14 @@
+/**
+ * @file            pokemonism/ralts/kirlia/gardevoir/node.cc
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 02, 2025
+ */
+
+
+#include "node.hh"
+
+namespace pokemonism {
+}
