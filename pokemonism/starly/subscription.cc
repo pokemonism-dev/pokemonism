5adcd972b45c9622e9fc2908d31db698e3eb8176
snorlax <snorlax@pokemonism.dev>
2025-09-02 10:53:10 +0900

    Update Engine.

---
+++ pokemonism/starly/subscription.cc
@@ -0,0 +1,16 @@
+/**
+ * @file            pokemonism/starly/subscription.cc
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 02, 2025
+ */
+
+#include "subscription.hh"
+
+namespace pokemonism {
+    namespace starly {
+
+    }
+}
