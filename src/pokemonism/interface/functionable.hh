364f9ee78d950f5460115b961c07809c7e54e436
snorlax <snorlax@pokemonism.dev>
2025-09-03 20:50:40 +0900

    Update Engine.

---
+++ src/pokemonism/interface/functionable.hh
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 03, 2025
+ */
+
+#ifndef   __POKEMONISM_FUNCTIONABLE_HH__
+#define   __POKEMONISM_FUNCTIONABLE_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism {
+    namespace interface {
+
+        struct functionable {};
+
+    }
+
+    using functionable = interface::functionable;
+
+}
+
+#endif // __POKEMONISM_FUNCTIONABLE_HH__
