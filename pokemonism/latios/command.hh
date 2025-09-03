6582b3a9dadb583c853c612d4e18082a425cab32
snorlax <snorlax@pokemonism.dev>
2025-09-03 17:27:14 +0900

    Update Engine.

--- pokemonism/latios/command.hh
+++ pokemonism/latios/command.hh
@@ -1,92 +1,25 @@
-// /**
-//  * @file
-//  * @brief
-//  * @details
-//  *
-//  * @author          snorlax <snorlax@pokemonism.dev>
-//  * @since           8월 31, 2025
-//  */
-//
-// #ifndef   __POKEMONISM_LATIOS_COMMAND_HH__
-// #define   __POKEMONISM_LATIOS_COMMAND_HH__
-//
-// #include <pokemonism.hh>
-//
-// #include <pokemonism/wattrel/command.hh>
-// #include <pokemonism/pokemon/generic/command.hh>
-// #include <pokemonism/latios/subscription.hh>
-//
-// #include "pokemon/event.hh"
-//
-// namespace pokemonism {
-//     namespace latios {
-//
-//         namespace command {
-//             struct tag {};
-//
-//
-//             template <class messageable = wattrel::command::envelope::message>
-//             class envelope : public wattrel::command::envelope, public pokemon::generic::command<messageable>::envelope {
-//             public:     typedef messageable             message;
-//             public:     inline message * pop(void) override { return dynamic_cast<messageable *>(output); }
-//             public:     inline envelope(void) {}
-//             public:     inline explicit envelope(pokemon::exception * e) : wattrel::command::envelope(e) {}
-//             public:     inline ~envelope(void) override {}
-//             public:     envelope(const envelope & o) = delete;
-//             public:     envelope(envelope && o) noexcept = delete;
-//             public:     envelope & operator=(const envelope & o) = delete;
-//             public:     envelope & operator=(envelope && o) noexcept = delete;
-//             };
-//         }
-//
-//         template <class objectable>
-//         class subscription<objectable, typename objectable::output, pokemon::command::tag> : public virtual wattrel::subscription {
-//         public:     constexpr static const char * name = "command";   // ### 20250901 | REMOVE THIS FOR DEBUG
-//         public:     explicit subscription(uint32 properties) : wattrel::subscription(properties) {}
-//         public:     subscription(void) = delete;
-//         public:     ~subscription(void) override {}
-//         public:     subscription(const subscription & o) = delete;
-//         public:     subscription(subscription && o) noexcept = delete;
-//         public:     subscription & operator=(const subscription & o) = delete;
-//         public:     subscription & operator=(subscription && o) noexcept = delete;
-//         };
-//
-//         namespace command {
-//
-//             template <typename objectable> class subscription;
-//
-//             class generator : public wattrel::command::generator {
-//             public:     template <class outputable, class objectable = pokemon::generic::command<outputable>> latios::command::subscription<objectable> * reg(objectable * target, uint32 properties, objectable::callback::type * callbacks, uint32 n);
-//             public:     inline explicit generator(wattrel::engine * engine) : wattrel::command::generator(engine) {}
-//             public:     generator(void) = delete;
-//             public:     inline ~generator(void) override {}
-//             public:     generator(const generator & o) = delete;
-//             public:     generator(generator && o) noexcept = delete;
-//             public:     generator & operator=(const generator & o) = delete;
-//             public:     generator & operator=(generator && o) noexcept = delete;
-//             };
-//
-//             template <typename objectable>
-//             class subscription : public latios::subscription<objectable>, public wattrel::command::subscription {
-//             public:     constexpr static const char * name = "generic subscription";
-//             public:     typedef objectable      command;
-//             public:     inline command * objectGet(void) const override { return dynamic_cast<command *>(object); }
-//             public:     inline explicit subscription(command * object, uint32 properties, const command::callback::type * callbacks, uint32 n) :
-//                         latios::subscription<objectable>(properties),
-//                         wattrel::command::subscription(object, reinterpret_cast<const wattrel::command::callback::type *>(callbacks), n),
-//                         wattrel::subscription(properties) {
-//
-//                         }
-//             public:     subscription(void) = delete;
-//             public:     inline ~subscription(void) override {}
-//             public:     subscription(const subscription & o) = delete;
-//             public:     subscription(subscription && o) noexcept = delete;
-//             public:     subscription & operator=(const subscription & o) = delete;
-//             public:     subscription & operator=(subscription && o) noexcept = delete;
-//             };
-//         }
-//
-//     }
-// }
-//
-// #endif // __POKEMONISM_LATIOS_COMMAND_HH__
+/**
+ * @file
+ * @brief
+ * @details
+ *
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           8월 31, 2025
+ */
+
+#ifndef   __POKEMONISM_LATIOS_COMMAND_HH__
+#define   __POKEMONISM_LATIOS_COMMAND_HH__
+
+#include <pokemonism.hh>
+
+#include "pokemon/event.hh"
+
+namespace pokemonism {
+    namespace latios {
+        namespace command {
+
+        }
+    }
+}
+
+#endif // __POKEMONISM_LATIOS_COMMAND_HH__
--- pokemonism/latios/engine.hh
+++ pokemonism/latios/engine.hh
@@ -1,83 +1,24 @@
-// /**
-//  * @file
-//  * @brief
-//  * @details
-//  *
-//  * @author          snorlax <snorlax@pokemonism.dev>
-//  * @since           8월 31, 2025
-//  */
-//
-// #ifndef   __POKEMONISM_LATIOS_ENGINE_HH__
-// #define   __POKEMONISM_LATIOS_ENGINE_HH__
-//
-// #include <pokemonism.hh>
-//
-// #include <pokemonism/wattrel/engine.hh>
-// #include <pokemonism/pokemon/generic/command.hh>
-//
-// namespace pokemonism {
-//     namespace latios {
-//
-//         namespace command {
-//             template <typename objectable> class subscription;
-//         }
-//
-//         class engine : protected wattrel::engine {
-//         public:     static pokemon::interface::terminatable<1, latios::engine *> T;
-//         protected:  static latios::engine * singleton;
-//         public:     typedef void (*bootstrapper)(latios::engine & o);
-//         public:     typedef void (*cancellation)(latios::engine * o);
-//         public:     static void t1000(latios::engine * o);
-//         public:     static void on(latios::engine::bootstrapper bootstrap, wattrel::engine::bootstrapper wattrel);
-//         public:     static void off(latios::engine::cancellation t1000, wattrel::engine::cancellation t800);
-//         public:     static void off(pokemon::interface::terminator<latios::engine *> t1000, pokemon::interface::terminator<wattrel::engine *> t800);
-//         public:     static int run(int n);
-//         public:     template <class outputable, typename objectable = pokemon::generic::command<outputable>> static latios::command::subscription<objectable> * commandReg(objectable * target, uint32 properties, objectable::callback::type * callbacks, uint32 n);
-//         protected:  engine(void);
-//         protected:  ~engine(void) override;
-//         public:     engine(const engine & o) = delete;
-//         public:     engine(engine && o) noexcept = delete;
-//         public:     engine & operator=(const engine & o) = delete;
-//         public:     engine & operator=(engine && o) noexcept = delete;
-//         };
-//
-//     }
-// }
-//
-// #include <pokemonism/latios/command.hh>
-//
-// namespace pokemonism {
-//     namespace latios {
-//         namespace command {
-//
-//             template <class outputable, class objectable>
-//             latios::command::subscription<objectable> * generator::reg(objectable * target, uint32 properties, typename objectable::callback::type * callbacks, uint32 n) {
-//                 latios::command::subscription<objectable> * subscription = new latios::command::subscription<objectable>(target, properties, callbacks, n);
-//
-//                 subscription->on(wattrel::command::event::type::gen);
-//
-//                 printf("call\n");
-//
-//                 if (add(subscription) != declaration::success) return allocator::del(subscription);
-//
-//                 wattrel::command::event * event = new wattrel::command::event(wattrel::command::event::type::execute, new wattrel::command::node(subscription));
-//
-//                 engine->push(event);
-//
-//                 return subscription;
-//             }
-//
-//         }
-//
-//         template <class outputable, typename objectable>
-//         latios::command::subscription<objectable> * engine::commandReg(objectable * target, uint32 properties, typename objectable::callback::type * callbacks, uint32 n) {
-//             if (singleton == nullptr) throw pokemon::exception();
-//
-//             latios::command::generator * o = dynamic_cast<latios::command::generator *>(singleton->generator.command);
-//
-//             return o->reg<outputable, objectable>(target, properties, callbacks, n);
-//         }
-//     }
-// }
-//
-// #endif // __POKEMONISM_LATIOS_ENGINE_HH__
+/**
+ * @file
+ * @brief
+ * @details
+ *
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           8월 31, 2025
+ */
+
+#ifndef   __POKEMONISM_LATIOS_ENGINE_HH__
+#define   __POKEMONISM_LATIOS_ENGINE_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism {
+    namespace latios {
+
+
+
+    }
+}
+
+
+#endif // __POKEMONISM_LATIOS_ENGINE_HH__
