f4e78d29ec4aae5ba466dbbd6d30f942a206d468
snorlax <snorlax@pokemonism.dev>
2025-09-03 20:51:59 +0900

    Update Engine.

---
+++ src/pokemonism/interface/callable.hh
@@ -0,0 +1,23 @@
+/**
+ * @file            pokemonism/interface/callable.hh
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 03, 2025
+ */
+
+#ifndef   __POKEMONISM_INTERFACE_CALLABLE_HH__
+#define   __POKEMONISM_INTERFACE_CALLABLE_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism {
+    namespace interface {
+
+        struct callable {};
+
+    };
+}
+
+#endif // __POKEMONISM_INTERFACE_CALLABLE_HH__
