2b9c10798c378dc7c2155bf749d91b1065672542
snorlax <snorlax@pokemonism.dev>
2025-09-04 05:07:16 +0900

    Update Engine.

---
+++ src/pokemonism/callable.hh
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 04, 2025
+ */
+
+#ifndef   __POKEMONISM_CALLABLE_HH__
+#define   __POKEMONISM_CALLABLE_HH__
+
+#include <pokemonism/primitivable.hh>
+
+namespace pokemonism {
+
+    template <typename outputable = primitivable>
+    class callable {
+    public:     virtual outputable operator()(void) = 0;
+    public:     callable(void) {}
+    public:     virtual ~callable(void) {}
+    public:     callable(const callable<outputable> & o) = delete;
+    public:     callable(callable<outputable> && o) noexcept = delete;
+    public:     callable<outputable> & operator=(const callable<outputable> & o) = delete;
+    public:     callable<outputable> & operator=(callable<outputable> && o) noexcept = delete;
+    };
+}
+
+#endif // __POKEMONISM_CALLABLE_HH__
---
+++ src/pokemonism/functionable.hh
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 04, 2025
+ */
+
+#ifndef   __POKEMONISM_FUNCTIONABLE_HH__
+#define   __POKEMONISM_FUNCTIONABLE_HH__
+
+#include <pokemonism/primitivable.hh>
+
+namespace pokemonism {
+
+    template <typename outputable = primitivable, typename... argumentable>
+    class functionable {
+    public:     virtual outputable operator()(argumentable... args) = 0;
+    public:     functionable(void) {}
+    public:     virtual ~functionable(void) {}
+    public:     functionable(const functionable<outputable, argumentable...> & o) = delete;
+    public:     functionable(functionable<outputable, argumentable...> && o) noexcept = delete;
+    public:     functionable<outputable, argumentable...> & operator=(const functionable<outputable, argumentable...> & o) = delete;
+    public:     functionable<outputable, argumentable...> & operator=(functionable<outputable, argumentable...> && o) noexcept = delete;
+    };
+
+}
+
+#endif // __POKEMONISM_FUNCTIONABLE_HH__
