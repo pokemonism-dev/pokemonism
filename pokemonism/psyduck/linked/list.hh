313a61e643d77c44c339d71480e48e7fd2e09425
snorlax <snorlax@pokemonism.dev>
2025-09-04 10:28:40 +0900

    Update Engine.

---
+++ pokemonism/psyduck/linked/list.hh
@@ -0,0 +1,209 @@
+/**
+ * @file            pokemonism/psyduck/linked/list.hh
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           Sep 4, 2025
+ */
+
+#ifndef   __POKEMONISM_PSYDUCK_LINKED_LIST_HH__
+#define   __POKEMONISM_PSYDUCK_LINKED_LIST_HH__
+
+// ReSharper disable once CppUnusedIncludeDirective
+#include <pokemonism/psyduck.hh>
+#include <pokemonism/allocator.hh>
+
+namespace pokemonism {
+
+    template <class collection, class element>
+    element * psyduck::linked::list<collection, element>::add(collection * container, element * node) {
+        if (container == nullptr || node == nullptr) throw pokemonism::exception();
+        if (node->container != nullptr) throw pokemonism::exception();
+
+        node->prev = container->tail;
+        if (node->prev) {
+            node->prev->next = node;
+        } else {
+            container->head = node;
+        }
+        container->tail = node;
+        container->size = container->size + 1;
+        node->container = container;
+
+        return nullptr;
+    }
+
+    template <class collection, class element>
+    element * psyduck::linked::list<collection, element>::add(collection * container, element * node, synchronizable & sync) {
+        if (container == nullptr || node == nullptr) throw pokemonism::exception();
+        if (node->container != nullptr) throw pokemonism::exception();
+
+        sync.lock();
+        node->prev = container->tail;
+        if (node->prev) {
+            node->prev->next = node;
+        } else {
+            container->head = node;
+        }
+        container->tail = node;
+        container->size = container->size + 1;
+        node->container = container;
+        sync.unlock();
+
+        return nullptr;
+    }
+
+    template <class collection, class element>
+    element * psyduck::linked::list<collection, element>::del(collection * container, element * node) {
+        if (container == nullptr || node == nullptr) throw pokemonism::exception();
+        if (node->container != container) throw pokemonism::exception();
+
+        element * prev = node->prev;
+        element * next = node->next;
+
+        if (prev) {
+            prev->next = next;
+            node->prev = nullptr;
+        } else {
+            container->head = next;
+        }
+
+        if (next) {
+            next->prev = prev;
+            node->next = nullptr;
+        } else {
+            container->tail = prev;
+        }
+
+        container->size = container->size - 1;
+        node->container = nullptr;
+
+        return node;
+    }
+
+    template <class collection, class element>
+    element * psyduck::linked::list<collection, element>::del(collection * container, element * node, synchronizable & sync) {
+        if (container == nullptr || node == nullptr) throw pokemonism::exception();
+        if (node->container != container) throw pokemonism::exception();
+
+        sync.lock();
+        element * prev = node->prev;
+        element * next = node->next;
+
+        if (prev) {
+            prev->next = next;
+            node->prev = nullptr;
+        } else {
+            container->head = next;
+        }
+
+        if (next) {
+            next->prev = prev;
+            node->next = nullptr;
+        } else {
+            container->tail = prev;
+        }
+        container->size = container->size - 1;
+        node->container = nullptr;
+        sync.unlock();
+
+        return node;
+    }
+
+    template <class collection, class element>
+    element * psyduck::linked::list<collection, element>::pop(collection * container) {
+        if (container == nullptr) throw pokemonism::exception();
+
+        element * node = container->head;
+        if (node != nullptr) {
+            if (node->next) {
+                container->head = node->next;
+                node->next->prev = nullptr;
+                node->next = nullptr;
+            } else {
+                container->head = nullptr;
+                container->tail = nullptr;
+            }
+            container->size = container->size - 1;
+            node->container = nullptr;
+        }
+
+        return node;
+    }
+
+    template <class collection, class element>
+    element * psyduck::linked::list<collection, element>::pop(collection * container, synchronizable & sync) {
+        if (container == nullptr) throw pokemonism::exception();
+
+        sync.lock();
+        element * node = container->head;
+        if (node != nullptr) {
+            if (node->next) {
+                container->head = node->next;
+                node->next->prev = nullptr;
+                node->next = nullptr;
+            } else {
+                container->head = nullptr;
+                container->tail = nullptr;
+            }
+            container->size = container->size - 1;
+            node->container = nullptr;
+        }
+        sync.unlock();
+
+        return node;
+    }
+
+    template <class collection, class element>
+    void psyduck::linked::list<collection, element>::clear(collection * container) {
+        if (container == nullptr) throw pokemonism::exception();
+
+        while (element * node = list<collection, element>::pop(container)) allocator::del(node);
+    }
+
+    template <class collection, class element>
+    void psyduck::linked::list<collection, element>::clear(collection * container, synchronizable & sync) {
+        if (container == nullptr) throw pokemonism::exception();
+
+        sync.lock();
+        while (element * node = list<collection, element>::pop(container)) {
+            sync.unlock();
+            allocator::del(node);
+            sync.lock();
+        }
+        sync.unlock();
+    }
+
+    template <class collection, class element>
+    void psyduck::linked::list<collection, element>::clear(collection * container, element * (*rel)(element *)) {
+        if (container == nullptr) throw pokemonism::exception();
+
+        while (element * node = list<collection, element>::pop(container)) allocator::del(rel(node));
+    }
+
+    template <class collection, class element>
+    void psyduck::linked::list<collection, element>::clear(collection * container, element * (*rel)(element *), synchronizable & sync) {
+        if (container == nullptr) throw pokemonism::exception();
+
+        sync.lock();
+        while (element * node = list<collection, element>::pop(container)) {
+            sync.unlock();
+            allocator::del(rel(node));
+            sync.lock();
+        }
+        sync.unlock();
+    }
+
+    template <typename container, typename element>
+    inline psyduck::linked::list<container, element>::list(void) {
+
+    }
+
+    template <typename container, typename element>
+    inline psyduck::linked::list<container, element>::~list(void) {
+
+    }
+}
+
+#endif // __POKEMONISM_PSYDUCK_LINKED_LIST_HH__
