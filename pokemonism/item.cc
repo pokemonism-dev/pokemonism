d217c7b40bcd4312dd8af468807c20844b4a13b6
snorlax <snorlax@pokemonism.dev>
2025-09-05 13:43:37 +0900

    Update Engine.

---
+++ pokemonism/item.cc
@@ -0,0 +1,14 @@
+/**
+ * @file            pokemonism/item.cc
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           Sep 5, 2025
+ */
+
+
+#include "item.hh"
+
+namespace pokemonism {
+}
