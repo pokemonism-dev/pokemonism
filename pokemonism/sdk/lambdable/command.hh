eb1f0934db470fd8421f4bb936ca40e973b29435
snorlax <snorlax@pokemonism.dev>
2025-09-07 19:04:07 +0900

    Update Engine.

---
+++ pokemonism/sdk/lambdable/command.hh
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 07, 2025
+ */
+
+#ifndef   __POKEMONISM_SDK_LAMBDABLE_COMMAND_HH__
+#define   __POKEMONISM_SDK_LAMBDABLE_COMMAND_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism {
+}
+
+#endif // __POKEMONISM_SDK_LAMBDABLE_COMMAND_HH__
