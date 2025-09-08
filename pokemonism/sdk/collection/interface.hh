0ce80688053d437bfbe1a481890a0b4b59522086
snorlax <snorlax@pokemonism.dev>
2025-09-08 13:51:10 +0900

    Update Engine.

--- pokemonism/sdk/collection/interface.hh
+++ pokemonism/sdk/collection/interface.hh
@@ -10,13 +10,22 @@
 #ifndef   __POKEMONISM_SDK_COLLECTION_INTERFACE_HH__
 #define   __POKEMONISM_SDK_COLLECTION_INTERFACE_HH__
 
+#include <pokemonism.hh>
 #include <pokemonism/sdk/synchronizable.hh>
 
 namespace pokemonism::sdk::collection {
 
     class interface : public synchronizable {
-    public:     interface(void) {}
-    public:     ~interface(void) override {}
+    public:     inline int lock(void) override { return declaration::fail; }
+    public:     inline int unlock(void) override { return declaration::fail; }
+    public:     inline int wait(void) override { return declaration::fail; }
+    public:     inline int wakeup(void) override { return declaration::fail; }
+    public:     inline int wait(long second, long nano) override { return declaration::fail; }
+    public:     inline int wakeup(bool all) override { return declaration::fail; }
+    public:     virtual unsigned long sizeGet(void) = 0;
+    public:     virtual void clear(void) = 0;
+    public:     inline interface(void) {}
+    public:     inline ~interface(void) override {}
     public:     interface(const collection::interface & o) = delete;
     public:     interface(collection::interface && o) noexcept = delete;
     public:     collection::interface & operator=(const collection::interface & o) = delete;
