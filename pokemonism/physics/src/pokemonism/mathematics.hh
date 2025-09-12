2a93a2493f48d1c442405cceca2990dbe7b20153
snorlax <snorlax@pokemonism.dev>
2025-09-12 11:27:26 +0900

    Update Engine.

---
+++ pokemonism/physics/src/pokemonism/mathematics.hh
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 12, 2025
+ */
+
+#ifndef   __POKEMONISM_MATHEMATICS_HH__
+#define   __POKEMONISM_MATHEMATICS_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism::mathematics {
+
+    struct Tag : public pokemonism::Tag {};
+
+}
+
+#endif // __POKEMONISM_MATHEMATICS_HH__
