fbe135ecd38546fb42c1075d983ec7bc16541b2d
snorlax <snorlax@pokemonism.dev>
2025-09-12 23:59:51 +0900

    Update Engine.

---
+++ pokemonism/window/src/pokemonism/window/interface/keyboard.cc
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 12, 2025
+ */
+
+
+#include "keyboard.hh"
+
+#include "keyboard/on.hh"
+
+namespace pokemonism::window::interface {
+
+    KeyboardOn * Keyboard::add(KeyboardOn * observer) {
+        return Collection::add(this, observer);
+    }
+
+    KeyboardOn * Keyboard::del(KeyboardOn * observer) {
+        return Collection::del(this, observer);
+    }
+
+    void Keyboard::clear(void) {
+        Collection::clear(this);
+    }
+
+    Keyboard::Keyboard(void) : size(declaration::zero), head(nullptr), tail(nullptr) {
+
+    }
+
+    Keyboard::~Keyboard(void) {
+        Collection::clear(this);
+    }
+
+}
