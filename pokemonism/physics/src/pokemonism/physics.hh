feecd6ccb45c51bd1fc3223133c7aa17251475f0
snorlax <snorlax@pokemonism.dev>
2025-09-12 10:32:20 +0900

    Update Engine.

---
+++ pokemonism/physics/src/pokemonism/physics.hh
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 12, 2025
+ */
+
+#ifndef   __POKEMONISM_PHYSICS_HH__
+#define   __POKEMONISM_PHYSICS_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism::physics {
+
+    struct Tag : public pokemonism::Tag {};
+
+}
+
+#endif // __POKEMONISM_PHYSICS_HH__
