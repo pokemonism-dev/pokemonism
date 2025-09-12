3efc000e45379017d6f9d92bef286a1b4c40c6ad
snorlax <snorlax@pokemonism.dev>
2025-09-12 10:34:45 +0900

    Update Engine.

---
+++ pokemonism/physics/src/pokemonism/physics/axis.hh
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * @brief
+ * @details
+ * 
+ * @author          snorlax <snorlax@pokemonism.dev>
+ * @since           9월 12, 2025
+ */
+
+#ifndef   __POKEMONISM_PHYSICS_AXIS_HH__
+#define   __POKEMONISM_PHYSICS_AXIS_HH__
+
+#include <pokemonism.hh>
+
+namespace pokemonism::physics {
+
+    class Axis {
+    public:     inline Axis(void);
+    public:     inline virtual ~Axis(void);
+    public:     Axis(const Axis & o) = delete;
+    public:     Axis(Axis && o) noexcept = delete;
+    public:     Axis & operator=(const Axis & o) = delete;
+    public:     Axis & operator=(Axis && o) noexcept = delete;
+    };
+
+    inline Axis::Axis(void) {
+
+    }
+
+    inline Axis::~Axis(void) {
+
+    }
+
+}
+
+#endif // __POKEMONISM_PHYSICS_AXIS_HH__
