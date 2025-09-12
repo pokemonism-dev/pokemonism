8db026c8d003a9219de93c8fe751ea7b37cd196f
snorlax <snorlax@pokemonism.dev>
2025-09-12 12:15:38 +0900

    Update Engine.

--- pokemonism/mathematics/src/pokemonism/mathematics/linear/algebra/vector.cc
+++ pokemonism/mathematics/src/pokemonism/mathematics/linear/algebra/vector.cc
@@ -6,3 +6,9 @@
  * @author          snorlax <snorlax@pokemonism.dev>
  * @since           9월 12, 2025
  */
+
+
+#include "vector.hh"
+
+namespace pokemonism {
+}
--- pokemonism/mathematics/src/pokemonism/mathematics/scalar.hh
+++ pokemonism/mathematics/src/pokemonism/mathematics/scalar.hh
@@ -2,7 +2,7 @@
  * @file
  * @brief
  * @details
- * 
+ *
  * @author          snorlax <snorlax@pokemonism.dev>
  * @since           9월 12, 2025
  */
